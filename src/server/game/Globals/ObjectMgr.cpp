92cf62ef9880c7ab5c250ac3ef0396c227e515b5
Nay <dnpd.dd@gmail.com>
2011-09-05 22:18:12 +0800

    Core/Quests: Implement quests that require PlayerSlain. Closes #169

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -4343,6 +4343,8 @@ void ObjectMgr::LoadQuests()
             mExclusiveQuestGroups.insert(std::pair<int32, uint32>(qinfo->ExclusiveGroup, qinfo->GetQuestId()));
         if (qinfo->LimitTime)
             qinfo->SetFlag(QUEST_TRINITY_FLAGS_TIMED);
+        if (qinfo->PlayersSlain)
+            qinfo->SetFlag(QUEST_TRINITY_FLAGS_PLAYER_KILL);
     }
 
     // check QUEST_TRINITY_FLAGS_EXPLORATION_OR_EVENT for spell with SPELL_EFFECT_QUEST_COMPLETE
