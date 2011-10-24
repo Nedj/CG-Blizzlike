a4468a7ff062ca52ec878220648592613031e73e
Magic <priadit@hotmail.com>
2011-10-24 16:26:49 +0800

    Death Knight Pets, General Things

--- sql/cgcustoms/DK_Pet_Immunities.sql
+++ sql/cgcustoms/DK_Pet_Immunities.sql
@@ -1,2 +1,2 @@
-UPDATE `creature_template` SET `mechanic_immune_mask` = "197136" WHERE `entry` = "26125";
-UPDATE `creature_template` SET `mechanic_immune_mask` = "197136" WHERE `entry` = "27829";
+UPDATE `creature_template` SET `mechanic_immune_mask` = "8454672" WHERE `entry` = "26125";
+UPDATE `creature_template` SET `mechanic_immune_mask` = "8454672" WHERE `entry` = "27829";
