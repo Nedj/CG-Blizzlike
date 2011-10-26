9664b83b1ccac9da5a4059492181292d909040ce
OPCODE187 <josh_r2@yahoo.com>
2011-10-26 03:20:23 -0700

    change spelleffect charge a little

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -6192,9 +6192,16 @@ void Spell::EffectCharge(SpellEffIndex /*effIndex*/)
         if (!unitTarget)
             return;
 
-        float x, y, z;
-        unitTarget->GetContactPoint(m_caster, x, y, z);
-        m_caster->GetMotionMaster()->MoveCharge(x, y, z);
+        //float x, y, z;
+        //unitTarget->GetContactPoint(m_caster, x, y, z);
+        //m_caster->GetMotionMaster()->MoveCharge(x, y, z);
+		float angle = target->GetRelativeAngle(m_caster);	
+        Position pos;	
+	
+        target->GetContactPoint(m_caster, pos.m_positionX, pos.m_positionY, pos.m_positionZ);	
+        target->GetFirstCollisionPosition(pos, target->GetObjectSize(), angle);	
+	
+        m_caster->GetMotionMaster()->MoveCharge(pos.m_positionX, pos.m_positionY, pos.m_positionZ + target->GetObjectSize());
     }
 
     if (effectHandleMode == SPELL_EFFECT_HANDLE_HIT_TARGET)
