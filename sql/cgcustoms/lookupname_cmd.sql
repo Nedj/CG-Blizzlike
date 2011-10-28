10ed55b731e8ccb9c6adacc65429a3393f48c4a6
OPCODE187 <josh_r2@yahoo.com>
2011-10-28 05:15:53 -0700

    sql for .lookup player name command

---
+++ sql/cgcustoms/lookupname_cmd.sql
@@ -0,0 +1,4 @@
+INSERT INTO `command` (`name`, `security`, `help`) VALUES
+('lookup player name', 3, 'Syntax: .lookup player name $name ($limit) 
+
+ Searchs players, for name specified, and returns accounts and chars of specified name or your target .');
